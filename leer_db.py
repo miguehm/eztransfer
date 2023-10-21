72dfe0e4fbfa6ec9746aef0578ad0372fb3e305b
miguehm <miguel.hernandezmor@alumno.buap.mx>
2023-10-21 12:08:24 -0600

    rework: append code to read_by_fle() func

--- leer_db.py
+++ leer_db.py
@@ -1,25 +1,30 @@
 import sqlite3
 
-filename = input("Ingrese el nombre del archivo: ")
+def read_by_file():
+    
+    filename = input("Ingrese el nombre del archivo: ")
 
-conexion = sqlite3.connect('test.db')
+    conexion = sqlite3.connect('test.db')
 
-cursor = conexion.cursor()
+    cursor = conexion.cursor()
 
-cursor.execute(f'SELECT * FROM archives WHERE filename LIKE ?', ('%'+filename+'%', ))
+    cursor.execute(f'SELECT * FROM archives WHERE filename LIKE ?', ('%'+filename+'%', ))
 
 # registro = cursor.fetchone()
-resultados = cursor.fetchall()
+    resultados = cursor.fetchall()
 
-print("-------------------------")
-if resultados is not None:
-    for registro in resultados:
-        print(f"Filename: {registro[1]}")
-        print(f"Download link: https://transfer.sh/{registro[0]}/{registro[1]}")
-        print(f"Delete token: {registro[2]}")
-        print(f"Upload date: {registro[3]}/{registro[4]}/{registro[5]} - {registro[6]}:{registro[7]}:{registro[8]}")
-        print("-------------------------")
-else:
-    print("No se encontró registro")
+    print("-------------------------")
+    if resultados is not None:
+        for registro in resultados:
+            print(f"Filename: {registro[1]}")
+            print(f"Download link: https://transfer.sh/{registro[0]}/{registro[1]}")
+            print(f"Delete token: {registro[2]}")
+            print(f"Upload date: {registro[3]}/{registro[4]}/{registro[5]} - {registro[6]}:{registro[7]}:{registro[8]}")
+            print("-------------------------")
+    else:
+        print("No se encontró registro")
 
-conexion.close()
+    conexion.close()
+
+if __name__ == "__main__":
+    read_by_file()
