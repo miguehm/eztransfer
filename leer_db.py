fe897f072307c33666d9c0cdb9f4b40f0f20447a
miguehm <miguel.hernandezmor@alumno.buap.mx>
2023-12-05 18:21:00 -0600

    add show all files

--- leer_db.py
+++ leer_db.py
@@ -13,18 +13,41 @@ def read_by_file():
 # registro = cursor.fetchone()
     resultados = cursor.fetchall()
 
-    print("-------------------------")
+    print(f"\n= Mostrando resultados para la búsqueda \"{filename}\" \n")
     if resultados is not None:
         for registro in resultados:
             print(f"Filename: {registro[0]}")
             print(f"Download link: https://transfer.sh/{registro[1]}/{registro[0]}")
             print(f"Delete token: {registro[2]}")
             print(f"Upload date: {registro[3]}/{registro[4]}/{registro[5]} - {registro[6]}:{registro[7]}:{registro[8]}")
+            print(f"Max days: {registro[9]}")
             print("-------------------------")
     else:
         print("No se encontró registro")
 
     conexion.close()
 
+def show_files():
+    conexion = sqlite3.connect('test.db')
+
+    cursor = conexion.cursor()
+
+    cursor.execute('SELECT * FROM archives')
+
+    resultados = cursor.fetchall()
+
+    # TODO: show as tables
+    if resultados is not None:
+        for registro in resultados:
+            print(f"Filename: {registro[0]}")
+            print(f"Download link: https://transfer.sh/{registro[1]}/{registro[0]}")
+            print(f"Delete token: {registro[2]}")
+            print(f"Upload date: {registro[3]}/{registro[4]}/{registro[5]} - {registro[6]}:{registro[7]}:{registro[8]}")
+            print(f"Max days: {registro[9]}")
+            print("-------------------------")
+
+    conexion.close()
+
 if __name__ == "__main__":
     read_by_file()
+    # show_files()
